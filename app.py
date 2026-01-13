c2cb290c9cbafde7001347747a6cdfa74f263fd2
Muhammad-Awaix <mawais.ai021@gmail.com>
2026-01-13 10:27:54 +0500

    delete operation

--- app.py
+++ app.py
@@ -114,3 +114,12 @@ def create_patient(patient: Patient):
     save_data(data)
 
     return JSONResponse(status_code=201, content={'message':'patient created successfully'})
+
+@app.delete('/delete/{patient_id}')
+def delete_patient(patient_id:str):
+    data = load_data()
+    if patient_id not in data:
+        raise HTTPException(status_code=404, detail='Patient not found')
+    del data[patient_id]
+    save_data(data)
+    return JSONResponse(status_code=200, content={'message':'Patient deleted successfully'})
